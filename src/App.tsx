443f5845d6e29e311a966637c4ccf7d365d41687
milosj1992 <milosje1992@gmail.com>
2023-10-06 21:24:38 +0200

    adding jest test

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,27 @@ import { Suspense } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
 import Loader from './components/Loader';
+/**
+ * 
+ * @returns "@testing-library/jest-dom": "^6.1.3",
+  "@testing-library/react": "^14.0.0",
+  "@testing-library/user-event": "^13.5.0",
+  "@types/jest": "^29.5.5",
+  "@types/testing-library__jest-dom": "^6.0.0",
+  "jest": "^29.7.0",
+  "jest-environment-jsdom": "^29.7.0",
+  "ts-jest": "^29.1.1",
+  "ts-node": "^10.9.1",
+  "typescript": "^5.0.2"
+  "identity-obj-proxy": "^3.0.0",
 
+ */
 const App = () => {
-
   return (
     <Suspense fallback={<Loader />}>
       <RouterProvider router={router} />
     </Suspense>
-  )
-
-}
+  );
+};
 
 export default App;
---
+++ src/__tests__/AppTest.test.tsx
@@ -0,0 +1,29 @@
+// Imports
+import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+
+// To Test
+import App from '../AppTest';
+
+// Tests
+test('Renders main page correctly', async () => {
+  // Setup
+  render(<App />);
+  const buttonCount = await screen.findByRole('button');
+  const codeCount = await screen.queryByText(/The count is now:/);
+
+  // Pre Expecations
+  expect(buttonCount.innerHTML).toBe('count is: 0');
+  // Instead of:
+  // expect(codeCount).toBeNull();
+  expect(codeCount).not.toBeInTheDocument();
+
+  // Init
+  user.click(buttonCount);
+  user.click(buttonCount);
+
+  // Post Expectations
+  expect(buttonCount.innerHTML).toBe('count is: 2');
+  expect(await screen.queryByText(/The count is now:/)).toBeInTheDocument();
+});
